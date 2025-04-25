214c6383abf945c31f69dca73359b16f1a22784d
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-04-25 22:30:46 +0500

    update

---
+++ chapter_2/Variables.py
@@ -0,0 +1,33 @@
+print("""
+(1) what is varibale?
+In Python, a variable is like a container that holds data or a value. 
+You can think of it as a name that refers to something stored in the computer's memory.
+
+x = 10
+name = "Muhammad Asif"
+is_active = True
+
+x is a variable that holds the number 10
+name holds the string "Muhammad Asif"
+is_active holds the Boolean value True
+""")
+
+
+print("""
+(2) Rules for Naming Variables:
+Must start with a letter or an underscore (_)
+
+Cannot start with a number
+
+Can only contain letters, numbers, and underscores
+
+Case-sensitive (name and Name are different)
+
+Avoid using Python keywords as variable names (like if, class, for, etc.)
+
+Examples of valid variable names:
+
+age = 25
+_first_name = "Ali"
+user_123 = "John"
+""")
---
+++ chapter_2/Variables_2.py
@@ -0,0 +1,12 @@
+student_name = "Muhammad Asif"
+roll_no = 2253
+semester = 6
+university = "University of Peshawar"
+department = "BS Computer Science"
+
+print("Name :",student_name)
+print("Roll number :",roll_no)
+print("Semester :",semester)
+print("University :",university)
+print("Department :",department)
+
