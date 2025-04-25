8c73266b7069110cec27867a210419ca93f03e94
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-04-25 22:58:53 +0500

    Create Strings.py

---
+++ chapter_3/Strings.py
@@ -0,0 +1,14 @@
+print("""
+In Python, a string is a data type used to store **textual information**. 
+It is a sequence of characters such as letters, numbers, symbols, and spaces. 
+Strings are enclosed in quotes (either single, double, or triple quotes) and are one of the most commonly used 
+data types in Python programming.
+
+Strings are used to represent words, sentences, messages, file names, and any other type of text data. 
+Python treats each character in the string as a separate element, which means you can access, manipulate, 
+and analyze individual characters or groups of characters within a string.
+
+Since text is an essential part of most programs, understanding how strings work is a 
+fundamental part of learning Python.
+      
+""")
