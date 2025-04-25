68c72d437f009b3c5a2aeffb3a9bed35a08f2c3d
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-04-25 23:01:44 +0500

    updates

--- chapter_3/Strings.py
+++ chapter_3/Strings.py
@@ -1,4 +1,6 @@
 print("""
+what is string?
+
 In Python, a string is a data type used to store **textual information**. 
 It is a sequence of characters such as letters, numbers, symbols, and spaces. 
 Strings are enclosed in quotes (either single, double, or triple quotes) and are one of the most commonly used 
@@ -8,7 +10,4 @@ Strings are used to represent words, sentences, messages, file names, and any ot
 Python treats each character in the string as a separate element, which means you can access, manipulate, 
 and analyze individual characters or groups of characters within a string.
 
-Since text is an essential part of most programs, understanding how strings work is a 
-fundamental part of learning Python.
-      
 """)
