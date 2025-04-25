6fb19c4e57cfbcb11760399b8472a8dfdd966486
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-04-25 22:39:30 +0500

    updates

--- chapter_2/Variables_2.py
+++ chapter_2/Variables_2.py
@@ -1,12 +1,16 @@
-student_name = "Muhammad Asif"
-roll_no = 2253
-semester = 6
-university = "University of Peshawar"
-department = "BS Computer Science"
+# Student Information
+student_name = "Muhammad Asif"             # Student's full name
+roll_no = 2253                              # Roll number
+semester = 6                                # Current semester
+university = "University of Peshawar"       # Name of the university
+department = "BS Computer Science"          # Department or program
 
-print("Name :",student_name)
-print("Roll number :",roll_no)
-print("Semester :",semester)
-print("University :",university)
-print("Department :",department)
+# Displaying the student information
+print("Student Profile")
+print("----------------------------")
+print("Name       :", student_name)
+print("Roll No    :", roll_no)
+print("Semester   :", semester)
+print("University :", university)
+print("Department :", department)
 
---
+++ chapter_2/Variables_3.py
@@ -0,0 +1,9 @@
+# Student Information
+student_name = "Muhammad Asif"
+roll_no = 2253
+semester = 6
+university = "University of Peshawar"
+department = "BS Computer Science"
+
+# Print all information in one line
+print(f"Name: {student_name} | Roll No: {roll_no} | Semester: {semester} | University: {university} | Department: {department}")
