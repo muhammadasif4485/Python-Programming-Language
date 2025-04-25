19744a6c1a0f75285bd3b0c77e829fa778b6320c
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-04-25 22:02:13 +0500

    Create python_definition.py

---
+++ Chapter_1/python_definition.py
@@ -0,0 +1,51 @@
+print("""
+💡What is Programming?
+Programming is the process of giving instructions to a computer to perform specific tasks. 
+These instructions are written in a programming language like Python, Java, C++, etc.
+
+Think of it like this:
+
+💬Programming is how humans "talk" to computers to make them do what we want.
+      """)
+
+
+print("""
+💡What is Python?
+Python is a high-level, interpreted programming language that is known for its simplicity, readability, and versatility. It was created by Guido van Rossum and released in 1991.
+
+It's used in a wide range of fields like:
+
+Web development
+
+Data science
+
+Machine learning & AI
+
+Automation & scripting
+
+Game development
+
+Cybersecurity
+
+Software testing
+""")
+
+print("""
+Why use Python?
+
+Python is loved by beginners and experts alike. Here's why:
+
+Easy to learn & use – Clean syntax, almost like English.
+
+Perfect for beginners – Gentle learning curve.
+
+Versatile – Works across many domains (AI, web, data science, etc.).
+
+Huge job demand – Widely used in the tech industry.
+
+Rich ecosystem – Tons of libraries & frameworks (e.g., NumPy, Pandas, Django, Flask, TensorFlow).
+
+Strong community – Massive global support & tutorials.
+
+
+""")
