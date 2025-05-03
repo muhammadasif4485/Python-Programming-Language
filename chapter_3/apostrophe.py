23330a3100335e18a15a1ac7b2faf75d8500b898
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-05-04 00:28:36 +0500

    Update apostrophe.py

--- chapter_3/apostrophe.py
+++ chapter_3/apostrophe.py
@@ -1,2 +1,6 @@
+# In Python, you can use apostrophes (single quotes) inside a string by either
 message = "One of Python's strengths is its diverse community."
 print(message)
+
+text = "It's a beautiful day!"
+print(text)
