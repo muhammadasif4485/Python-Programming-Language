1fac52fe12a695eb66549fc9b035ad60f195a681
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-05-04 01:34:20 +0500

    Create comment_2.py

---
+++ chapter_3/comment_2.py
@@ -0,0 +1,20 @@
+# Single-Line Comments
+# This is the first comment
+# This is the second comment
+# This is the third comment
+
+
+# Multi-Line (Block) Comment Using Triple Quotes
+'''
+This is a block comment
+that spans multiple lines.
+It won't be executed or assigned,
+so it acts like a comment.
+'''
+# Multi-Line (Block) Comment Using Triple Quotes
+"""
+This is a block comment
+that spans multiple lines.
+It won't be executed or assigned,
+so it acts like a comment.
+"""
