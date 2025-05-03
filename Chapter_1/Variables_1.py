c571547433764bd11a25c370bb9128f24d08fa5f
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-05-04 01:30:34 +0500

    Update Variables_1.py

--- Chapter_1/Variables_1.py
+++ Chapter_1/Variables_1.py
@@ -1,5 +1,10 @@
 message = "Hello Python world!"
 print(message)
+# The variable message is assigned the string value "Hello Python world!".
+# The print() function then outputs the value of message, which is Hello Python world!.
+
 
 message = "Hello Python Crash Course world!"
 print(message)
+# The same variable message is reassigned a new string value: "Hello Python Crash Course world!".
+# The print() function now outputs the updated value of message, which is Hello Python Crash Course world!.
