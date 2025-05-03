4f08f82982c9714b66cecf9137ba11f7b943cf2d
MUHAMMAD ASIF <asifnasaaz2005@gmail.com>
2025-05-04 01:32:02 +0500

    Update Variables_1.py

--- Chapter_1/Variables_1.py
+++ Chapter_1/Variables_1.py
@@ -8,3 +8,14 @@ message = "Hello Python Crash Course world!"
 print(message)
 # The same variable message is reassigned a new string value: "Hello Python Crash Course world!".
 # The print() function now outputs the updated value of message, which is Hello Python Crash Course world!.
+
+
+''' 
+Key Concepts:
+
+Variables: message is a variable that stores string values. You can change (reassign) its value anytime.
+
+print(): A built-in Python function that displays the value passed to it (in this case, the variable message).
+
+Dynamic Typing: Python allows variables to be reassigned to different data types, though here it's just strings.
+'''
