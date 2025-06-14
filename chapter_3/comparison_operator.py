d323e10ac15ee9ba3ef7f5fce1f9ef5f8f405be0
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-06-14 07:51:39 +0500

    Add files via upload

---
+++ chapter_3/comparison_operator.py
@@ -0,0 +1,27 @@
+#  this is callled Comparison Operators:
+#  ==  Equal To
+#  != Not Equal To
+#  > Greater then 
+#  < less then
+#  >= Greater then or equal to
+#  <= less then or equal to
+
+# step 1
+''' 
+a = 10 
+b = 5
+print(a == b)
+print(a > b)
+print(a < b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+'''
+# Step 2
+
+age_limit = 18
+my_age = 19
+
+print(my_age > age_limit)
+
+# Step 3 
